ba7dcbd9297ce1b951414bd43130062d5c372ef6
Gabosv3 <gabriel503.alegria@gmail.com>
2024-09-23 22:19:49 -0600

    Tarea completa typescript

--- Actividad-Typescript/main.js
+++ Actividad-Typescript/main.js
@@ -4,12 +4,26 @@ obtenga el título, color y fuente de la página, el segundo método que indique
 aparezca el título si centrado, derecha o izquierda y el tercer método que imprima todas las
 propiedades.
  */
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var LibroCabecera = /** @class */ (function () {
-    function LibroCabecera(tituloParam, colorParam, fuenteParam, alineacionParam) {
+    function LibroCabecera(tituloParam, colorParam, fuenteParam) {
         this.Titulo = tituloParam;
         this.Color = colorParam;
         this.Fuente = fuenteParam;
-        this.setAlineacion(alineacionParam);
     }
     /** Getter and Setter */
     /** Getter */
@@ -36,7 +50,7 @@ var LibroCabecera = /** @class */ (function () {
         this.Fuente = fuenteParam;
     };
     LibroCabecera.prototype.setAlineacion = function (alineacionParam) {
-        var tipoAlineaciones = ['centrado', 'derecha', 'izquierda'];
+        var tipoAlineaciones = ["centrado", "derecha", "izquierda"];
         if (tipoAlineaciones.indexOf(alineacionParam) !== -1) {
             this.Alineacion = alineacionParam;
         }
@@ -46,8 +60,9 @@ var LibroCabecera = /** @class */ (function () {
     };
     return LibroCabecera;
 }());
-var paginita = new LibroCabecera("Mi Página", "azul", "Arial", "centrado");
-paginita.setAlineacion("jueves");
+var paginita = new LibroCabecera("Mi Página", "azul", "Arial");
+console.log("--------------");
+paginita.setAlineacion("centrado");
 console.log("EJercicio 1");
 console.log(paginita);
 console.log("--------------");
@@ -106,4 +121,133 @@ console.log("Divisi\u00F3n por cero: ".concat(calculadora.Dividir(5, 0)));
 console.log("Potencia: ".concat(calculadora.potencia(2, 3)));
 console.log("Factorial de 5: ".concat(calculadora.Factorial(5)));
 console.log("Factorial de -1: ".concat(calculadora.Factorial(-1)));
+console.log("--------------");
+/**EJERCICIO 3. Crea una clase llamada Canción:
+Atributos: título, género de la canción y un atributo privado que se llame autor.
+Métodos:
+• Crear un constructor que reciba como parámetros el título y género de la canción.
+• Utiliza los métodos get y set para recibir e imprimir la propiedad autor.
+• Crea un método para mostrar los datos de la canción.
+ */
+var cancion = /** @class */ (function () {
+    function cancion(tituloParam, generoParam) {
+        this.titulo = tituloParam;
+        this.genero = generoParam;
+    }
+    cancion.prototype.getAutor = function () {
+        return this.autor;
+    };
+    cancion.prototype.setAutor = function (autorParam) {
+        this.autor = autorParam;
+    };
+    return cancion;
+}());
+var rolitamix2024 = new cancion("la cancion", "pop");
+rolitamix2024.setAutor("Bad bunny");
+console.log("EJercicio 2");
+console.log(rolitamix2024);
+console.log(rolitamix2024.getAutor());
+console.log("--------------");
+/**EJERCICIO 4. Crea una clase llamada Cuenta y va contener lo siguiente:
+
+Atributos: nombre, cantidad, tipo de cuenta y número de cuenta.
+Métodos:
+• Crear un constructor que reciba como parámetros el nombre, cantidad, tipo de cuenta y
+número de cuenta.
+• Crea un método llamado depositar y en base a la cantidad ingresada en el constructor haz una
+condición de que si la cantidad es menor a $5.00 que se imprima un mensaje que el valor a
+depositar debe ser mayor a $5.00 y sino solo imprima un mensaje de que se ha depositado
+correctamente y la cantidad depositada.
+• Crea un método llamado retirar, que reciba un parámetro llamado valor y en base a eso tienes
+que restar la cantidad menos el valor ingresado e imprimir un mensaje de lo que ha retirado y
+cuanto le queda en su cuenta.
+Nota: Para el método retirar debes de validar que lo que se retire sea mayor de $5.00 ya que
+si no hay efectivo debes de tirar un mensaje que no hay nada en la cuenta.
+• Crea un método para mostrar los datos de su nombre, tipo de cuenta y número de cuenta.
+• Define un objeto de la clase Cuenta y llama sus métodos. */
+var cuenta = /** @class */ (function () {
+    function cuenta(nombreParam, cantidadParam, numeroCuentaParam, tipodeCuentaParam) {
+        this.nombre = nombreParam;
+        this.cantidad = cantidadParam;
+        this.numeroCuenta = numeroCuentaParam;
+        this.tipodeCuenta = tipodeCuentaParam;
+    }
+    cuenta.prototype.Deposito = function (cantidadParam) {
+        if (cantidadParam < 5) {
+            console.log("No se puede depositar esa cantidad");
+        }
+        else {
+            this.cantidad = this.cantidad + cantidadParam;
+            console.log("Deposito exitoso");
+        }
+    };
+    cuenta.prototype.retiro = function (cantidadParam) {
+        if (cantidadParam < 5) {
+            console.log("No se puede retirar esa cantidad");
+        }
+        else if (this.cantidad - cantidadParam < 0) {
+            console.log("No se puede retirar esa cantidad");
+        }
+        else {
+            this.cantidad = this.cantidad - cantidadParam;
+            console.log("Retiro exitoso");
+        }
+    };
+    cuenta.prototype.datos = function () {
+        console.log("Nombre: ".concat(this.nombre));
+        console.log("Tipo de cuenta: ".concat(this.tipodeCuenta));
+        console.log("Numero de cuenta: ".concat(this.numeroCuenta));
+    };
+    return cuenta;
+}());
+var cuenta1 = new cuenta("Gabriel", 100, "123456", "corriente");
+console.log("EJercicio 4");
+cuenta1.Deposito(20);
+cuenta1.retiro(10);
+cuenta1.datos();
+console.log("--------------");
+var Persona = /** @class */ (function () {
+    function Persona(nombreParam, apellidoParam, direccionParam, telefonoParam, edadParam) {
+        this.nombre = nombreParam;
+        this.apellido = apellidoParam;
+        this.direccion = direccionParam;
+        this.telefono = telefonoParam;
+        this.edad = edadParam;
+    }
+    Persona.prototype.imprimiredad = function () {
+        if (this.edad < 18) {
+            console.log("Eres menor de edad");
+        }
+        else {
+            console.log("Eres mayor de edad");
+        }
+    };
+    return Persona;
+}());
+var empleado = /** @class */ (function (_super) {
+    __extends(empleado, _super);
+    function empleado(nombreParam, apellidoParam, direccionParam, telefonoParam, edadParam, sueldaParam) {
+        var _this = _super.call(this, nombreParam, apellidoParam, direccionParam, telefonoParam, edadParam) || this;
+        _this.sueldo = sueldaParam;
+        return _this;
+    }
+    empleado.prototype.cargasueldo = function (sueldoParam) {
+        this.sueldo = sueldoParam;
+    };
+    empleado.prototype.imprimirsueldo = function () {
+        console.log("Sueldo: ".concat(this.sueldo));
+    };
+    empleado.prototype.datosPersonales = function () {
+        console.log("Nombre: ".concat(this.nombre));
+        console.log("Apellido: ".concat(this.apellido));
+        console.log("Edad: ".concat(this.edad));
+    };
+    return empleado;
+}(Persona));
+var empleado1 = new empleado("Juan", "Pérez", "Calle 123", 55551234, 30, 50000);
+// Llamar a los métodos
+console.log("EJercicio 5");
+empleado1.datosPersonales();
+empleado1.imprimiredad();
+empleado1.imprimirsueldo();
 console.log("--------------");
--- Actividad-Typescript/main.ts
+++ Actividad-Typescript/main.ts
@@ -5,73 +5,68 @@ aparezca el título si centrado, derecha o izquierda y el tercer método que imp
 propiedades.
  */
 
-class LibroCabecera{
-   private Titulo: string;
-   private Color:string;
-   private Fuente:string;
-   private Alineacion:string;
+class LibroCabecera {
+    private Titulo: string;
+    private Color: string;
+    private Fuente: string;
+    private Alineacion: string;
 
-   
-    constructor(tituloParam:string,colorParam:string,fuenteParam:string,alineacionParam: string){
+    constructor(tituloParam: string, colorParam: string, fuenteParam: string) {
         this.Titulo = tituloParam;
         this.Color = colorParam;
         this.Fuente = fuenteParam;
-        this.setAlineacion(alineacionParam);
     }
 
     /** Getter and Setter */
 
     /** Getter */
 
-    
-    getTitulo() : string {
-        return  this.Titulo;
+    getTitulo(): string {
+        return this.Titulo;
     }
 
-    getColor() : string {
-        return this.Color
+    getColor(): string {
+        return this.Color;
     }
-    getFuente() : string {
-        return this.Fuente
+    getFuente(): string {
+        return this.Fuente;
     }
-    getAlineacion() : string {
-        return this.Alineacion
+    getAlineacion(): string {
+        return this.Alineacion;
     }
-
 
     /** Setter */
-    
-    setTitutlo(tituloParam : string) {
+
+    setTitutlo(tituloParam: string) {
         this.Titulo = tituloParam;
     }
 
-    setColor(colorParam : string) {
+    setColor(colorParam: string) {
         this.Color = colorParam;
     }
-    setFuente(fuenteParam : string) {
+    setFuente(fuenteParam: string) {
         this.Fuente = fuenteParam;
     }
-    
+
     setAlineacion(alineacionParam: string) {
-        const tipoAlineaciones: string[] = ['centrado', 'derecha', 'izquierda'];
+        const tipoAlineaciones: string[] = ["centrado", "derecha", "izquierda"];
         if (tipoAlineaciones.indexOf(alineacionParam) !== -1) {
             this.Alineacion = alineacionParam;
         } else {
-            console.error("Debe elegir entre las alineaciones: centrado, derecha o izquierda");
+            console.error(
+                "Debe elegir entre las alineaciones: centrado, derecha o izquierda"
+            );
         }
     }
-
-
-    
 }
 
-let paginita = new LibroCabecera("Mi Página", "azul", "Arial","centrado");
-console.log("--------------")
-paginita.setAlineacion("jueves");
-console.log("EJercicio 1")
+let paginita = new LibroCabecera("Mi Página", "azul", "Arial");
+console.log("--------------");
+paginita.setAlineacion("centrado");
+console.log("EJercicio 1");
 
-console.log(paginita)
-console.log("--------------")
+console.log(paginita);
+console.log("--------------");
 
 /**EJERCICIO 2: Crear una clase Calculadora que contendrá los siguientes métodos:
 • Sumar
@@ -82,23 +77,22 @@ console.log("--------------")
 • Factorial
  */
 
-class Calculadora{
-
-    Sumar(num1Param:number,num2Param:number):number{
+class Calculadora {
+    Sumar(num1Param: number, num2Param: number): number {
         return num1Param + num2Param;
     }
 
-    Restar(num1Param:number,num2Param:number):number{
+    Restar(num1Param: number, num2Param: number): number {
         return num1Param - num2Param;
     }
 
-    Dividir(num1Param:number,num2Param:number):number{
+    Dividir(num1Param: number, num2Param: number): number {
         return num1Param - num2Param;
     }
-    Multiplicar(num1Param:number,num2Param:number):number | string{
-       if(num2Param === 0 ){
+    Multiplicar(num1Param: number, num2Param: number): number | string {
+        if (num2Param === 0) {
             return "no se puede dividir entre 0";
-       }
+        }
         return num1Param + num2Param;
     }
 
@@ -107,7 +101,7 @@ class Calculadora{
     }
 
     Factorial(NumParam: number): number | string {
-        if ( NumParam< 0) {
+        if (NumParam < 0) {
             return "Error: El factorial no está definido para números negativos";
         }
         if (NumParam === 0 || NumParam === 1) {
@@ -119,12 +113,11 @@ class Calculadora{
         }
         return resultado;
     }
-
 }
 
 // Ejemplo de uso
 const calculadora = new Calculadora();
-console.log("EJercicio 2")
+console.log("EJercicio 2");
 console.log(`Suma: ${calculadora.Sumar(5, 3)}`);
 console.log(`Resta: ${calculadora.Restar(5, 3)}`);
 console.log(`Multiplicación: ${calculadora.Multiplicar(5, 3)}`);
@@ -133,5 +126,176 @@ console.log(`División por cero: ${calculadora.Dividir(5, 0)}`);
 console.log(`Potencia: ${calculadora.potencia(2, 3)}`);
 console.log(`Factorial de 5: ${calculadora.Factorial(5)}`);
 console.log(`Factorial de -1: ${calculadora.Factorial(-1)}`);
-console.log("--------------")
+console.log("--------------");
+
+/**EJERCICIO 3. Crea una clase llamada Canción:
+Atributos: título, género de la canción y un atributo privado que se llame autor.
+Métodos: 
+• Crear un constructor que reciba como parámetros el título y género de la canción.
+• Utiliza los métodos get y set para recibir e imprimir la propiedad autor. 
+• Crea un método para mostrar los datos de la canción.
+ */
+
+class cancion {
+    public titulo: string;
+    public genero: string;
+    private autor: string;
+
+    constructor(tituloParam: string, generoParam: string) {
+        this.titulo = tituloParam;
+        this.genero = generoParam;
+    }
+
+    getAutor(): string {
+        return this.autor;
+    }
+
+    setAutor(autorParam: string) {
+        this.autor = autorParam;
+    }
+}
+
+let rolitamix2024 = new cancion("la cancion", "pop");
+rolitamix2024.setAutor("Bad bunny");
+console.log("EJercicio 2");
+console.log(rolitamix2024);
+console.log(rolitamix2024.getAutor());
+console.log("--------------");
+
+/**EJERCICIO 4. Crea una clase llamada Cuenta y va contener lo siguiente:
+
+Atributos: nombre, cantidad, tipo de cuenta y número de cuenta.
+Métodos: 
+• Crear un constructor que reciba como parámetros el nombre, cantidad, tipo de cuenta y 
+número de cuenta.
+• Crea un método llamado depositar y en base a la cantidad ingresada en el constructor haz una 
+condición de que si la cantidad es menor a $5.00 que se imprima un mensaje que el valor a 
+depositar debe ser mayor a $5.00 y sino solo imprima un mensaje de que se ha depositado 
+correctamente y la cantidad depositada.
+• Crea un método llamado retirar, que reciba un parámetro llamado valor y en base a eso tienes 
+que restar la cantidad menos el valor ingresado e imprimir un mensaje de lo que ha retirado y 
+cuanto le queda en su cuenta.
+Nota: Para el método retirar debes de validar que lo que se retire sea mayor de $5.00 ya que 
+si no hay efectivo debes de tirar un mensaje que no hay nada en la cuenta.
+• Crea un método para mostrar los datos de su nombre, tipo de cuenta y número de cuenta. 
+• Define un objeto de la clase Cuenta y llama sus métodos. */
+
+class cuenta {
+    private nombre: string;
+    private cantidad: number;
+    private numeroCuenta: string;
+    private tipodeCuenta: string;
+
+    constructor(nombreParam: string,cantidadParam: number,numeroCuentaParam: string,tipodeCuentaParam: string) {
+        this.nombre = nombreParam;
+        this.cantidad = cantidadParam;
+        this.numeroCuenta = numeroCuentaParam;
+        this.tipodeCuenta = tipodeCuentaParam;
+    }
+
+    Deposito(cantidadParam: number) {
+        if (cantidadParam < 5) {
+            console.log("No se puede depositar esa cantidad");
+        } else {
+            this.cantidad = this.cantidad + cantidadParam;
+            console.log("Deposito exitoso");
+        }
+    }
+
+    retiro(cantidadParam: number) {
+        if (cantidadParam < 5) {
+            console.log("No se puede retirar esa cantidad");
+        } else if (this.cantidad - cantidadParam < 0) {
+            console.log("No se puede retirar esa cantidad");
+        } else {
+            this.cantidad = this.cantidad - cantidadParam;
+            console.log("Retiro exitoso");
+        }
+    }
+
+    datos() {
+        console.log(`Nombre: ${this.nombre}`);
+        console.log(`Tipo de cuenta: ${this.tipodeCuenta}`);
+        console.log(`Numero de cuenta: ${this.numeroCuenta}`);
+    }
+
+}
+
+let cuenta1 = new cuenta("Gabriel", 100, "123456", "corriente");
+console.log("EJercicio 4");
+cuenta1.Deposito(20);
+cuenta1.retiro(10);
+cuenta1.datos();
+console.log("--------------");
+
+/**JERCICIO 5. Crear una clase abstracta Persona y va contener lo siguiente:
+Atributos: nombre, apellido, dirección, teléfono y edad.
+Métodos: 
+• Crear un método constructor para recibir los datos.
+• Crea un método que en base a la edad imprima un mensaje si es mayor de edad o no.
+• Crea un método para mostrar todos los datos personales (será el método abstracto).
+• Crea una clase extra llamada Empleado y va contener un atributo llamado sueldo.
+• En la clase Empleado añade los métodos cargar sueldo e imprimir sueldo.
+• La clase Empleado va heredar de la clase Persona.
+• Define un objeto de la clase Empleado y que se imprima los datos del empleado y su sueldo.
+ */
+abstract class Persona {
+
+    protected nombre: string;
+    protected apellido: string;
+    protected direccion: string;
+    protected telefono: number;
+    protected edad: number;
+
+    constructor (nombreParam: string, apellidoParam: string, direccionParam: string, telefonoParam: number, edadParam: number) {
+        this.nombre = nombreParam;
+        this.apellido = apellidoParam;
+        this.direccion = direccionParam;
+        this.telefono = telefonoParam;
+        this.edad = edadParam;
+    }
+
+    imprimiredad(): void {
+        if (this.edad < 18) {
+            console.log("Eres menor de edad");
+        } else {
+            console.log("Eres mayor de edad");
+        }
+    }
+
+    abstract datosPersonales(): void;
+}
+
+class empleado extends Persona {
+    private sueldo: number;
+
+    constructor (nombreParam: string, apellidoParam: string, direccionParam: string, telefonoParam: number, edadParam: number, sueldaParam: number) {
+        super (nombreParam, apellidoParam, direccionParam, telefonoParam, edadParam);
+        this.sueldo = sueldaParam;
+    }
+
+    cargasueldo(sueldoParam: number): void {
+        this.sueldo = sueldoParam;
+    }
+
+    imprimirsueldo(): void {
+        console.log(`Sueldo: ${this.sueldo}`);
+    }
+
+    datosPersonales(): void {
+        console.log(`Nombre: ${this.nombre}`);
+        console.log(`Apellido: ${this.apellido}`);
+        console.log(`Edad: ${this.edad}`);
+    } 
+}
+
+let empleado1 = new empleado("Juan", "Pérez", "Calle 123", 55551234, 30, 500);
+
+// Llamar a los métodos
+console.log("EJercicio 5");
+empleado1.datosPersonales();
+empleado1.imprimiredad();
+empleado1.imprimirsueldo();
+console.log("--------------");
+
 
